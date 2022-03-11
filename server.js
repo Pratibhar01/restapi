23b61d2a1b49c69586a8c579cec400c0a33a82dd
suneeltechnew <suneel.s@hyconsoft.in>
2022-03-11 23:30:21 +0530

    test: deployment

--- server.js
+++ server.js
@@ -1,44 +1,55 @@
 const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
-const morgan = require('morgan'); 
+// const morgan = require('morgan'); 
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME;
+
+// const userRoute = require('./route/user');
+// const chits = require('./route/chits');
 
 
-const userRoute = require('./route/user');
-const chits = require('./route/chits');
 
+// mongoose.connect( 'mongodb://127.0.0.1:27017/')
+//   .then(() => console.log("DB Connection Successfull!"))
+//   .catch((err)=>{
+//       console.log(err);
+//   })
 
+app.use(cors());
 
-mongoose.connect( 'mongodb://127.0.0.1:27017/')
-  .then(() => console.log("DB Connection Successfull!"))
-  .catch((err)=>{
-      console.log(err);
-  })
-  
-    app.use(cors());
-
-const PORT = process.env.PORT || 5000;
-    app.listen(PORT, ()=>{
+app.listen(PORT, HOSTNAME, ()=>{
     console.log("Backend server is running! " + PORT);
 })
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.set("view engine","ejs")
+app.use('/', (req, res, next) => {
+    res.statusCode = 200;
+    res.send('you visited server...');
+})
+
+app.use('/hello', (req, res, next) => {
+    res.statusCode = 200;
+    res.send('hello from server');
+})
+
+// app.set("view engine","ejs")
 //app.set("views",path.resolve(__dirname,"views/ejs"))
 
-app.use(express.static(path.resolve(__dirname)))
-app.use(express.static(path.resolve(__dirname)))
+// app.use(express.static(path.resolve(__dirname)))
+// app.use(express.static(path.resolve(__dirname)))
 
 
 
-app.use('/user',userRoute);
-app.use('/chits', chits);
+// app.use('/user',userRoute);
+// app.use('/chits', chits);
 
 app.use((req,res,next) =>{
     res.status(404).json({
