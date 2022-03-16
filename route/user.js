66bb6bb85cbe72c4ec2fb485d9567431985bdd5d
Pratibhar0102 <pratibha.hirepatt@gmail.com>
2022-03-16 09:06:41 +0530

    plan commit

--- route/user.js
+++ route/user.js
@@ -14,36 +14,49 @@ const jwt = require('jsonwebtoken');
 // router.get('/add-user',services.add_user);
 
 router.post('/signup',(req,res,next)=>{
-    bcrypt.hash(req.body.password,10,(err,hash)=>{
-         if(err)
-           {
-               return res.status(500).json({
-                   error:err
+    User.find({email:req.body.email})
+    .exec()
+    .then(user =>{
+        if(user.length >=1){
+            return res.status(200).json({
+                message:'mail exists'
+            })
+        }else{
+            bcrypt.hash(req.body.password,10,(err,hash)=>{
+                if(err)
+                  {
+                      return res.status(500).json({
+                          error:err
+                      })
+                  }
+                  else
+                  {
+                       const user = new User({
+                   _id: new mongoose.Types.ObjectId,
+                      username:req.body.username,
+                      password:hash,
+                      email:req.body.email
+                   
+               })
+           
+              user.save()
+               .then(result=>{
+                   console.log(result);
+                   res.status(200).json({
+                       message:'User created'
+                   });
+               })
+               .catch(err=>{
+                   console.log(err);
+                   res.status(500).json({
+                       error:err
+                   })
                })
            }
-           else
-           {
-                const user = new User({
-            _id: new mongoose.Types.ObjectId,
-               username:req.body.username,
-               password:hash,
-               email:req.body.email
-            
-        })
-    
-       user.save()
-        .then(result=>{
-            res.status(200).json({
-                new_user:result
-            })
-        })
-        .catch(err=>{
-            res.status(500).json({
-                error:err
-            })
-        })
-    }
+           })
+        }
     })
+   
 })
 
  router.post('/login',(req,res,next)=>{
