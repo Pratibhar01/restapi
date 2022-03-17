19d7424682c1f61ecb9714cde23278dd566a9f70
Pratibhar0102 <pratibha.hirepatt@gmail.com>
2022-03-17 13:51:13 +0530

    response commit

--- route/user.js
+++ route/user.js
@@ -13,7 +13,6 @@ const jwt = require('jsonwebtoken');
 
 // router.get('/add-user',services.add_user);
 
-<<<<<<< HEAD
 router.post('/signup',(req,res,next)=>{
     User.find({email:req.body.email})
     .exec()
@@ -61,102 +60,21 @@ router.post('/signup',(req,res,next)=>{
     })
    
 })
-
- router.post('/login',(req,res,next)=>{
-     User.findOne({email:req.body.email})
-     .exec()
-     .then(user=>{
-         console.log('result:', user);
-         if(user.length < 1)
-         {
-             return res.status(200).json({
-                 status:"10003",
-                 msg:'email not exist'
-             })
-         }      
-         bcrypt.compare(req.body.password,user.password,(err,result)=>{
-             if(!result)
-             {
-                 return res.status(200).json({
-                     status:"10004",
-                     msg:'password matching fail'
-                 })
-             }
-            if(result)
-            {
-                const token = jwt.sign({
-                   email :user.email
-                
-                 },
-                 'this is dummy text',
-                
-                  );
-                res.status(200).json({
-                    status:"10005",
-                    msg:"login successful",
-                    data:{
-                    email:user.email,
-                    token :token
-=======
-router.post('/signup', (req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(200).json({
-                    status: "10001",
-                    message: 'mail exists'
-                })
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        })
-                    }
-                    else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId,
-                            username: req.body.username,
-                            password: hash,
-                            email: req.body.email
-
-                        })
-
-                        user.save()
-                            .then(result => {
-                                console.log(result);
-                                res.status(200).json({
-                                    message: 'User created'
-                                });
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                res.status(500).json({
-                                    error: err
-                                })
-                            })
->>>>>>> b13787eb4bc7cc17d1a7e4eb9f3ddba3b30d61b0
-                    }
-                })
-            }
-        })
-
-})
-
 router.post('/login', (req, res, next) => {
     User.findOne({ email: req.body.email })
         .exec()
         .then(user => {
             if(!user) {
-                return res.status(401).json({
+                return res.status(200).json({
+                    status:"10003",
                     msg: 'email not exist'
                 })
             }
 
             bcrypt.compare(req.body.password, user.password, (err, result) => {
                 if (!result) {
-                    return res.status(401).json({
+                    return res.status(200).json({
+                        status:"10004",
                         msg: 'password matching fail'
                     })
                 }
@@ -167,8 +85,12 @@ router.post('/login', (req, res, next) => {
                     'this is dummy text',
                     );
                     res.status(200).json({
+                        status:"10005",
+                        msg:"login successful",
+                        data:{
                         email: user.email,
                         token: token
+                        }
                     })
                 }
             })
